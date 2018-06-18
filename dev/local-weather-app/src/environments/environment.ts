76520cdf349dc5cf51617e2258d85ff2c18f9893
THEWORLD\NgoChi <vinhngochi@hotmail.com>
2018-06-19 01:40:20 +0700

    initial sourcecode

---
+++ dev/local-weather-app/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appId: '074e50c61b6bbd5d451a552d97edf2ac',
+  baseUrl: 'http://',
+}
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
